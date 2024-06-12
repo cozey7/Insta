9e9bb4dcda41b2622314dc52e9e370933cd69f7f
Clarence Choy <clarence.z.choy@gmail.com>
2024-06-12 15:11:41 -0700

    Converted into executable (for windows)

--- ratio.py
+++ ratio.py
@@ -28,7 +28,7 @@ class Browser:
 
     def login_instagram(self, username: str, password: str):
         self.open_page("https://www.instagram.com")
-        time.sleep(3)
+        time.sleep(2)
         self.add_input(by=By.NAME, value="username", text=username)
         self.add_input(by=By.NAME, value="password", text=password)
         time.sleep(1)
@@ -39,23 +39,28 @@ class Browser:
     def get_followers(self, username: str):
         self.open_page(f"https://www.instagram.com/{username}/")
         time.sleep(3)
-        self.click_button(by=By.PARTIAL_LINK_TEXT, value="followers")
-        time.sleep(0.1)
 
+        #Click the followers
+        self.click_button(by=By.PARTIAL_LINK_TEXT, value="followers")
+        time.sleep(1)
+
+        #XPATH of the popup window
         scrollable_list = self.browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]")
         
+        #Getting all elements with certain username class
         followers = self.browser.find_elements(by=By.XPATH, value="//span[contains(@class, '_ap3a _aaco _aacw _aacx _aad7 _aade')]")
 
+        #Scrolling through to load more followers
         while True:
             last_count = len(followers)
             self.browser.execute_script("""arguments[0].scrollTop = arguments[0].scrollHeight""", scrollable_list)
-            time.sleep(1)  # Add a delay to allow time for the followers to load
+            time.sleep(2)  # Add a delay to allow time for the followers to load
             followers = self.browser.find_elements(by=By.XPATH, value="//span[contains(@class, '_ap3a _aaco _aacw _aacx _aad7 _aade')]")
             new_count = len(followers)
             if new_count == last_count:
                 break  # Exit the loop if no new followers are loaded
         
-        #Convert WebElement list into Strings
+        #Convert WebElement list into strings
         for i in range(len(followers)):
             followers[i] = followers[i].text
         
@@ -65,7 +70,7 @@ class Browser:
         self.open_page(f"https://www.instagram.com/{username}/")
         time.sleep(3)
         self.click_button(by=By.PARTIAL_LINK_TEXT, value="following")
-        time.sleep(0.1)
+        time.sleep(1)
 
         scrollable_list = self.browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[4]")
         
@@ -74,19 +79,22 @@ class Browser:
         while True:
             last_count = len(following)
             self.browser.execute_script("""arguments[0].scrollTop = arguments[0].scrollHeight""", scrollable_list)
-            time.sleep(1)  # Add a delay to allow time for the followers to load
+            time.sleep(2)  # Add a delay to allow time for the followers to load
             following = self.browser.find_elements(by=By.XPATH, value="//span[contains(@class, '_ap3a _aaco _aacw _aacx _aad7 _aade')]")
             new_count = len(following)
             if new_count == last_count:
                 break  # Exit the loop if no new followers are loaded
         
-        #Convert WebElement list into Strings
+        #Convert WebElement list into strings
         for i in range(len(following)):
             following[i] = following[i].text
         
         return following
     
     def print_celebs_fans(self, followers, following):
+        followers.sort()
+        following.sort()
+
         celebs = []
         fans = []
 
@@ -111,15 +119,15 @@ class Browser:
 
 def main() -> None:
     username = input("What is your Instagram username?\n")
-    password = input("What is your Instagram password? (This is not a scam)\n")
+    password = input("What is your Instagram password? (This is not a hack)\n")
+    print("Checking your ratio now...\n")
     browser = Browser('drivers\chromedriver.exe')
     browser.login_instagram(username, password)
     time.sleep(3)
     followers = browser.get_followers(username)
     following = browser.get_following(username)
-    followers.sort()
-    following.sort()
     browser.print_celebs_fans(followers, following)
+    print(f"You have {len(followers)} followers and {len(following)} following")
     browser.close_browser()
     print("This window will close in 2 minutes. Take a screenshot if needed.")
     time.sleep(120)
